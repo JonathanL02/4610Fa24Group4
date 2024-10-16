6291c4c7d328277262e56d94cefb8c3cd9f6abdc
JonathanL02 <88793777+JonathanL02@users.noreply.github.com>
2024-10-16 15:37:42 -0400

    Add files via upload

--- Advanced Queries.sql
+++ Advanced Queries.sql
@@ -6,17 +6,15 @@ JOIN Members_Taking_Sessions ON Sessions.sessionID = Members_Taking_Sessions.ses
 GROUP BY className
 ORDER BY COUNT(DISTINCT(memberID)) DESC;
 
-SELECT membershipType, COUNT(classID)
-FROM Memberships
-JOIN Members ON Memberships.membershipID = Members.membershipID
-JOIN Members_Taking_Sessions ON Members.memberID = Members_Taking_Sessions.memberID
-GROUP BY membershipType;
+CALL TP_Q1();
 
 #Finds which members are not taking any classes
 SELECT memberFName, memberLName
 FROM Members
 WHERE NOT EXISTS (SELECT memberID FROM Members_Taking_Sessions WHERE Members.memberID = Members_Taking_Sessions.memberID)
 ORDER BY memberLName;
+
+CALL TP_Q3();
 
 #The number of classes that are above the average class duration
 SELECT COUNT(duration) AS "Number of sessions over average", className, AVG(duration) AS "Average session duration", 
@@ -26,9 +24,13 @@ JOIN Classes ON Sessions.classID = Classes.classID
 WHERE duration > (SELECT AVG(duration) FROM Sessions)
 GROUP BY className;
 
+CALL TP_Q2();
+
 #Finds the least popular membership to determine next steps (sunsetting the membership or adjusting it)
 SELECT membershipType, COUNT(memberID) AS "Number of members"
 FROM Memberships
 JOIN Members ON Memberships.membershipID = Members.membershipID
 GROUP BY membershipType
 ORDER BY COUNT(memberID); 
+
+CALL TP_Q4();
